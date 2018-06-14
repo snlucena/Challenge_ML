751036db9501d5c2a9458b7f24ae6aba0dfd9d56
snlucena <37759376+snlucena@users.noreply.github.com>
2018-06-14 09:21:50 -0300

    Add files via upload

---
+++ challenge_0.py
@@ -0,0 +1,33 @@
+import mysql.connector
+import imaplib
+import email
+
+cnx= mysql.connector.connect(host="127.0.0.1", user="root",
+passwd="**********", db="challenge_3")
+
+cursor = cnx.cursor()
+
+user = 'usuario@gmail.com'
+pwd = 'contraseña'
+
+def get_body(msg):
+    if msg.is_multipart():
+        return get_body(msg.get_payload(0))
+    else:
+        return msg.get_payload(None, True)
+
+
+
+server = imaplib.IMAP4_SSL("imap.gmail.com")
+server.login (user, pwd)
+server.select('INBOX')
+
+rslt, dat =server.uid('search', None, "ALL")
+inbox_item_list = dat[0].split()
+
+for i in inbox_item_list:
+    result, data = server.fetch(i, '(RFC822)')
+    raw = email.message_from_bytes(data[0][1])
+    body = (get_body(raw))
+    print(body)
+
